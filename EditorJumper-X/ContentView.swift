0467e1d5dc90eccad4e4a5d7e605bf4590d29299
Harold Gao <xiangaoole@outlook.com>
2025-05-23 16:47:37 +0800

    Initial Commit

---
+++ EditorJumper-X/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EditorJumper-X
+//
+//  Created by user on 2025/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EditorJumper-X/EditorJumper_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditorJumper_XApp.swift
+//  EditorJumper-X
+//
+//  Created by user on 2025/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EditorJumper_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
